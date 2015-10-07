14006b8fa76bb0af4a8676a47c8c43bc19e148b8
natalieogle <natalie.ogle85@gmail.com>
2015-10-06 19:30:20 -0700

    added login logic that works

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,26 @@ var router = express.Router();
 /* GET home page. */
 /*Currently, routes immediately to login screen, will change in the future to see home page if already logged in
 with use of cookies*/
+
+//login routes
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Warble' });
+  res.render('login', { title: 'Warbler' });
 });
 
 router.post('/', function(req, res, next) {
   var userLogin = req.body;
-  var name = userLogin.loginUserInput;
-  var userDatabase = database.get("users", name);
-  console.log(userLogin);
-  console.log(name);
-  console.log(userDatabase);
-  res.render('index', { title: 'Warble' });
+  var name1 = userLogin.loginUserInput;
+  var password1 = userLogin.loginPassInput;
+  var userDatabase = database.get("users", name1);
+  //if an object for this user's object is found in our database and password matches,
+  //render the main page (index.js).  If this user's object is NOT found and/or enters an
+  //incorrect password, then re-render the login.js page with an error that says their
+  //username or password is incorrect.
+  if (userDatabase !== undefined && password1 === userDatabase.password){
+    res.render('index', { title: 'Warbler', username: name1});
+  } else {
+    res.render('login', { title: 'Warbler', errMsg: 'Incorrect username or password.' });
+  }
 });
 
 module.exports = router;
