7c094a6e7fe13ff4790e0d065c264b196da0e9c6
natalieogle <natalie.ogle85@gmail.com>
2015-10-06 21:15:11 -0700

    Made separate register view and it works.

--- routes/index.js
+++ routes/index.js
@@ -7,24 +7,51 @@ var router = express.Router();
 with use of cookies*/
 
 //login routes
+//this is the first thing that loads on our page.  In the future, we can use cookies to get the index page
+//instead of the login page if user already signed in earlier (cookies...yum)
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Warbler' });
 });
 
+//this is the route that occurs when user hits login button
 router.post('/', function(req, res, next) {
   var userLogin = req.body;
-  var name1 = userLogin.loginUserInput;
-  var password1 = userLogin.loginPassInput;
-  var userDatabase = database.get("users", name1);
+  var name = userLogin.loginUserInput;
+  var password = userLogin.loginPassInput;
+  var userDatabase = database.get("users", name);
   //if an object for this user's object is found in our database and password matches,
   //render the main page (index.js).  If this user's object is NOT found and/or enters an
   //incorrect password, then re-render the login.js page with an error that says their
   //username or password is incorrect.
-  if (userDatabase !== undefined && password1 === userDatabase.password){
-    res.render('index', { title: 'Warbler', username: name1});
+  if (userDatabase !== undefined && password === userDatabase.password){
+    res.render('index', { title: 'Warbler', intro: 'Welcome back', username: name });
   } else {
     res.render('login', { title: 'Warbler', errMsg: 'Incorrect username or password.' });
   }
 });
+
+//register routes
+//this route renders the register page when user clicks the Sign Up button on the login page.
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Warbler' });
+});
+
+//this is the route that occurs when user clicks the Register button on the register page
+router.post('/users', function(req, res, next) {
+  var userRegister = req.body;
+  var name = userRegister.username;
+  var allUsers = database.list("users");
+  //if username is already in our database, console log a message saying the username is already taken.
+  //if username is not already taken, save username and password to database and take user to main (index) page.
+  if(name in allUsers) {
+    res.redirect('/register');
+    //right now we only console log this error message...not sure how to get it in browser yet.
+    console.log('Username is already taken.')
+  } else {
+    database.post("users", userRegister);
+    res.render('index', { title: 'Warbler', intro: "Welcome to Warbler", username: name });
+  }
+
+})
 
 module.exports = router;
