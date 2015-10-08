d0fd1f9aaa37157f81da18a19b8661d89ba46e7a
Stephen Printup <stephen.printup@gmail.com>
2015-10-07 20:27:51 -0700

    database work

--- orch.js
+++ orch.js
@@ -1,3 +1,49 @@
 var config = require('./config');
 var orch = require('orchestrate');
 var db = orch(config.dbkey);
+
+db.list('things').then(function(result) {
+	console.log(result.body.results)
+})
+
+console.log('hello, I am running')
+
+// db.post('things', {
+//   "name": "Steve Kaliski",
+//   "hometown": "New York, NY",
+//   "twitter": "@stevekaliski"
+// })
+// .then(function (result) {
+// console.log(result.path.key)
+// })
+
+
+// db.put('things', 'key', {
+//   "name": "Jake",
+//   "hometown": "Boston",
+//   "twitter": "@Jake"
+// })
+// .then(function (result) {
+
+// })
+// .fail(function (err) {
+
+// })
+
+
+// db.remove('things', 'key', true)
+// .then(function (result) {
+// console.log(result.body.results)
+
+// })
+// .fail(function (err) {
+
+// })
+
+db.get("things", "baratheon-robert", "20c14e8965d6cbb0")
+.then(function (result) {
+	console.log(result.path.key)
+})
+.fail(function (err) {
+
+})
